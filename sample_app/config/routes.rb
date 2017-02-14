15e7c6007cdd48efe673453039f691fdc1ba8486
connorotte <connorotte@gmail.com>
2017-02-14 00:17:34 +0000

    chapter 3 complete

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/newpage'
+  get 'static_pages/contact'
 
   get 'static_pages/home'
-#cool
+
   get 'static_pages/help'
 
   get 'static_pages/about'
 
-  root 'application#hello'
+  root 'static_pages#home'
 end
