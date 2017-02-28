dd2bd08e01cc7b34e6ac2203cde12fb36a5cc5da
connorotte <connorotte@gmail.com>
2017-02-28 07:12:59 +0000

    chapter 8 COMPLETED

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string
+#  email           :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string
+#
+
 class User < ApplicationRecord
     before_save { email.downcase! }
     validates :name, presence: true, length: { maximum: 50}
@@ -6,4 +18,10 @@ class User < ApplicationRecord
         format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false}
     has_secure_password
     validates :password, presence: true, length: { minimum: 5}
+    
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+    end
 end
