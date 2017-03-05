664ee1df4a711e2c14353d7ab8e6c658b8af7904
connorotte <connorotte@gmail.com>
2017-03-05 00:53:17 +0000

    10.2.2

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+   before_action :logged_in_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
@@ -25,11 +26,18 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      #handle success
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit'
     end
-    
+  end
+  
+  def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
   end
   
   
